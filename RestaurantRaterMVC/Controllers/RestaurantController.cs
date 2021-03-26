7d280a8e7c426ff7388d9511edd8cb36a471dab4
Ashley Sharpe <ashleysharpe00@gmail.com>
2021-03-26 18:44:26 -0500

    Details and Edit functionality and view added

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -63,5 +64,50 @@ namespace RestaurantRaterMVC.Controllers
             return RedirectToAction("Index");
         }
         
+        //Get: Restaurant/Edit/{id}
+        //Get id from the user
+        //Handle if null
+        //Find the restaurant by that id 
+        //if the restuarant doesn't exist
+        //Return the restaurant and the view 
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+
+        //Post: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+            }
+            return View(restaurant);
+        }
+        //GET Restaurant/Detail/{id}
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
